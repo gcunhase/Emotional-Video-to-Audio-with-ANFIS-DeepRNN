2c80497755389f58f5354874d993b8c27be59bda
gwena <gwena.cs@gmail.com>
2020-04-04 17:03:53 +0900

    Add: sound2feat test_individual file

--- scripts/features_from_sound/main_sound2feat_cognimuse_test_individual.m
+++ scripts/features_from_sound/main_sound2feat_cognimuse_test_individual.m
@@ -1,8 +1,7 @@
 %%All extension of the audio is used, not just the min
 %
 % Extracts audio features from COGNIMUSE dataset (TRAIN and TEST)
-%   Train: BMI, CHI, FNE, GLA, LOR
-%   Test: CRA, DEP
+%   Test_individual: CRA, DEP (features calculated for each video individually)
 %
 % Gwena Cunha
 %
@@ -12,8 +11,8 @@ clc
 
 %% Audio filename
 % Choose the files to extract the features from
-root_dir='dataset/cognimuse dataset/';
-list_of_clip_names = {'BMI', 'CHI', 'FNE', 'GLA', 'LOR', 'CRA', 'DEP'};
+root_dir='dataset/cognimuse dataset/test_individual/';
+list_of_clip_names = {'CRA', 'DEP'};
 list_of_clip_size = [];  % 625   602   605   600   750
 % Get numbers of videos in each clip
 for l=list_of_clip_names
@@ -87,55 +86,45 @@ for l=1:length(clip_struct_all_expanded)
     movie_id = clip_struct.movie_id;
     raw_data = clip_struct.raw_data;
     
-    save_root = 'saved_mats/cognimuse/features_from_sound/';
-    if strcmp(clip_name, 'CRA') || strcmp(clip_name, 'DEP')  % test
-        save_root = strcat([save_root, 'test/']);
-    else
-        save_root = strcat([save_root, 'train/']);
-    end
+    save_root = 'saved_mats/cognimuse/features_from_sound/test_individual/';
     mkdir( save_root);
     
-    save_to_filename = strcat([save_root, 'sound_features_dataset2_cognimuse_', clip_name, '_size', num2str(size_sound_feats), '_v7_raw']);
-
-    size_sound_feats_10 = size_sound_feats/10;
-    total_sound = zeros(30,size_sound_feats_10,clip_size);
-    sound_3D = zeros(30,size_sound_feats_10,clip_size);
-    sound_feat = [];
-    %feats_per_song = [225, 200, 210]; %225 because if I choose 230 there's an error with 
-    feats_per_song = 3*ones(1,clip_size); %50*ones(1,clip_size); %[100, 200, 225, 200, 180, 200, 200, 200];%, 210];
-    song_data = [];
     for j=1:clip_size
-        sound_feat_tmp = [];
-        song_data_tmp = [];
+        save_to_filename = strcat([save_root, 'sound_features_dataset2_cognimuse_', clip_name, '_video', num2str(j-1), '_size', num2str(size_sound_feats), '_v7_raw']);
+
+        size_sound_feats_10 = size_sound_feats/10;
+        total_sound = zeros(30,size_sound_feats_10,1);
+        sound_3D = zeros(30,size_sound_feats_10,1);
+        %feats_per_song = [225, 200, 210]; %225 because if I choose 230 there's an error with 
+        feats_per_song = 3; %50*ones(1,clip_size); %[100, 200, 225, 200, 180, 200, 200, 200];%, 210];
+        
         %feats_per_song = round(size(raw_data{j},1)/feats_size);
         val = size(raw_data{j},1);
-        for i=1:feats_per_song(j)
-            song = raw_data{j}((i-1)*val/feats_per_song(j)+1:i*val/feats_per_song(j));
-
-            %tempo = [t(1),t(2), ratio of t(1) and t(2)]
-            [tempo, loudness, rhythm] = sound_feature_gwena_withFeats(song, size_sound_feats);
-            % %% 3D Tensor data
-            D_sound=[tempo(:,1) loudness rhythm];
-            tensor_sound=zeros(10,3,size_sound_feats_10);
-
-            for k=1:size_sound_feats_10
-                tensor_sound(:,:,k)=D_sound(10*(k-1)+1:10*(k-1)+10,:);
-            end
-
-            for k=1:size_sound_feats_10
-                sound_3D(:,k,j,i)=[tensor_sound(:,1,k);tensor_sound(:,2,k);tensor_sound(:,3,k)];
-            end
-
-            %total_sound(:,:,j)=reshape(sound_3D,30*60,j);
-            sound_feat_tmp = [sound_feat_tmp, {D_sound}];
-            song_data_tmp = [song_data_tmp, song];
+        i=feats_per_song;
+        song = raw_data{j}((i-1)*val/i+1:i*val/i);
+            
+        %tempo = [t(1),t(2), ratio of t(1) and t(2)]
+        [tempo, loudness, rhythm] = sound_feature_gwena_withFeats(song, size_sound_feats);
+        % %% 3D Tensor data
+        D_sound=[tempo(:,1) loudness rhythm];
+        tensor_sound=zeros(10,3,size_sound_feats_10);
+            
+        for k=1:size_sound_feats_10
+            tensor_sound(:,:,k)=D_sound(10*(k-1)+1:10*(k-1)+10,:);
         end
-        sound_feat = [sound_feat; {sound_feat_tmp}];
-        song_data = [song_data; {song_data_tmp}];
-    end
+            
+        for k=1:size_sound_feats_10
+            sound_3D(:,k,j,i)=[tensor_sound(:,1,k);tensor_sound(:,2,k);tensor_sound(:,3,k)];
+        end
 
-    %%Save mat
-    %save sound_features_dataset2_size10 sound_feat sound_3D
-    %sound_feat -> 8x1 cells, each cell 10x3 features
-    save(save_to_filename, 'sound_feat', 'sound_3D', 'song_data', 'feats_per_song', 'movie_id', '-v7.3');
+        %total_sound(:,:,j)=reshape(sound_3D,30*60,j);
+        sound_feat = D_sound;
+        song_data = song;
+
+        %%Save mat
+        %save sound_features_dataset2_size10 sound_feat sound_3D
+        %sound_feat -> 8x1 cells, each cell 10x3 features
+        movie_id_individual = movie_id{j};
+        save(save_to_filename, 'sound_feat', 'sound_3D', 'song_data', 'feats_per_song', 'movie_id_individual', '-v7.3');
+    end
 end
