a6a7206790f90957b16047636db8664d2236edea
gwena <gwena.cs@gmail.com>
2020-04-05 21:26:52 +0900

    Update: comments, return Feature_train

--- scripts/features_from_visual/utils/mat2feat.m
+++ scripts/features_from_visual/utils/mat2feat.m
@@ -1,14 +1,26 @@
-function [] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, data_type, data_info)
-%MAT2FEAT converts videos saved in .mat files to visual features.
-%   data_info: for CONGMUSE data it's 'clip_struct', for others it's
-%   'movie_id'
+function [Feature_train] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, data_type, data_info)
+%MAT2FEAT converts videos saved in '.mat' files to visual features.
+%         Apply Fuzzy C-Means clustering (FCM)
+%
+%   Input:
+%      n_videos: number of videos in dataset
+%      data_to_load_filename: path/filename of '.mat' data
+%      save_feature_filename: cell structure with 2 filenames to save:
+%                             1: Feature_train, data_info
+%                             2: num_sort, number_items
+%      data_type: Options = ['cognimuse', 'deap', 'lindsey']. This is to
+%                 decide how to name the received 'data_info'
+%      data_info: for CONGMUSE data it's 'clip_struct', for others it's
+%                 'movie_id'
+%   Output:
+%      Feature_train: data features after applying FCM
+%
 
     load(data_to_load_filename);
-    %load EEG_GIST
-
-    %----------------random select ----------------------------
-    %3 videos
-    %n_videos = 38;
+    
+    % Shuffle videos in order to make the training unbiased. This
+    %   randomization vector is saved in order to sync with the emotion
+    %   scores later.
     [A num_sort] = sort(rand(1,n_videos));
     train_num = num_sort(1:n_videos);
 
@@ -17,9 +29,14 @@ function [] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, d
     train_L = L_data2{1}.train(:,train_num);
     train_O = O_data2{1}.train(:,:,train_num);
 
-    train_H_size = size(train_H); % n=10 (1000), n=125 (12500)
-    train_O_size = size(train_O); % n=10 (160), n=125 (2000)
-
+    % DEAP = {train: 2016, test: 176}
+    % COGNIMUSE: n=10 (160), n=125 (2000)
+    train_O_size = size(train_O);
+    
+    % DEAP = {train: 12600, test: 1100}
+    % COGNIMUSE: n=10 (1000), n=125 (12500)
+    train_H_size = size(train_H);
+    
     number_items = n_videos; %8 before
     for iter=1:number_items
         temp(:,:,iter) = train_O(:,:,iter)';
@@ -31,7 +48,7 @@ function [] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, d
 
     clear train_H train_S train_L train_O temp
 
-    %fcm: Fuzzy c-means clustering
+    % FCM: Fuzzy C-Means clustering
     %http://www.mathworks.com/matlabcentral/fileexchange/48686-fuzzyclustertoolbox/content/FuzzyClusterToolBox/FCM_Matlab/fcm.m
     %http://kr.mathworks.com/help/fuzzy/fcm.html
     [center_H,Y_h,obj_h_fcn] = fcm(train_H_data,3);
@@ -52,10 +69,6 @@ function [] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, d
     data_size = size(data_H_train);
     O_train_feature_ver2 = [];
 
-    %size(data_O_train)
-    %size(data_H_train)
-
-    %%---------------Changed until HERE-----------------------
     for k=1:number_items
         for i=1:data_size(2)
             distance_H(:,:,i) = distfcm(center_H,data_H_train(:,i,k));
@@ -98,8 +111,7 @@ function [] = mat2feat(n_videos, data_to_load_filename, save_feature_filename, d
     end
     Feature_train = temp;
 
-    %save feature_dataset2_1200 Feature_train
-    %save number_of_sort_dataset2_1200 num_sort number_items
+    % Save features
     if strcmp(data_type, 'cognimuse')
         clip_struct = data_info;
         save(save_feature_filename{1}, 'Feature_train', 'clip_struct', '-v7');
